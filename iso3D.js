834cbb3676dd408bee9b50b2408c3e8c79d2a293
JihadiJane <benjaminc.cone@gmail.com>
2017-10-25 18:37:07 -0400

    works in both Y+, Y- rotations, need to fix issue: moving rotPoint when starting from new position on other side

--- iso3D.js
+++ iso3D.js
@@ -1,10 +1,3 @@
-// get current values of corner pins
-// pick direction of face
-// pick end location and time
-//	// put a keyframe for each rotation
-// 	// always rotate on "lower" end
-//	//	// if sideways, non rotating edge becomes "lower"
-
 
 var mainComp = app.project.activeItem,  											// get the selected layer
 layerIndex = mainComp.selectedLayers[0].index,  								// grab index of selected layer
@@ -12,10 +5,11 @@ layer = mainComp.layer(layerIndex),
 temp,
 fps = 24,
 isoWidth = 50;
-pointA = [ 1250, 218],
-pointB = [ 960, 894],
-pointC = [ 389, 84],
-pointD = [ 1800, 970]
+pointA = [ 1250, 218 ],
+pointB = [ 960, 894 ],
+pointC = [ 389, 84 ],
+pointD = [ 1800, 970 ],
+selectedPoints = [ 0, 0, 0, 0 ]
 ;
 
 
@@ -24,28 +18,25 @@ var windowAEFKR = new Window ("dialog", "AEFKR"); 											// create window
 var groupInput = windowAEFKR.add ("group");													// create input groups
 groupInput.orientation = "column";
 
-var groupSelections = groupInput.add ("group");
-
-var groupRotZ = groupInput.add ("group");
-var rotZ = groupRotZ.add ("statictext", undefined, "Z rot:");									
-var inputRotZ = groupRotZ.add ("edittext", undefined, "Z");
-inputRotZ.characters = 4;
-groupRotZ.alignChildren = "center";
-
-var groupRotXY = groupInput.add ("group");
-var rotX = groupRotXY.add ("statictext", undefined, "X rot:");									
-var inputRotX = groupRotXY.add ("edittext", undefined, "X");
-var rotY = groupRotXY.add ("statictext", undefined, "Y rot:");	
-var inputRotY = groupRotXY.add ("edittext", undefined, "Y");
-inputRotX.characters = 4;						
-inputRotY.characters = 4;
-groupRotXY.alignChildren = "center";
+var groupSelectionsTop = groupInput.add ("group");
+var selection0 = groupSelectionsTop.add ("checkbox", undefined, "top left");
+var selection1 = groupSelectionsTop.add ("checkbox", undefined, "top right");
+var groupSelectionsBot = groupInput.add ("group");
+var selection2 = groupSelectionsBot.add ("checkbox", undefined, "bot left");
+var selection3 = groupSelectionsBot.add ("checkbox", undefined, "bot right");
 
 var groupIcon = groupInput.add("group");
 var current = File($.fileName).path;  
 var iconPath = current + "/icon.jpg"; 
+groupIcon.add ("image", undefined, iconPath);
 
-groupIcon.add ("image", undefined,iconPath);
+var groupRot = groupInput.add ("group");
+var rotAmount = groupRot.add ("statictext", undefined, "Amount to rot:");	
+var inputRot = groupRot.add ("edittext", undefined, "degrees");													
+inputRot.characters = 6;
+groupRot.alignChildren = "center";
+
+
 
 var groupButtons = windowAEFKR.add ("group");
 groupButtons.alignment = "right";
@@ -57,13 +48,19 @@ windowAEFKR.show();
 app.beginUndoGroup("undo script");
 buttonOK.onClick = execute(groupInput);
 app.endUndoGroup();
+
 //////////////////////////////////////////////////
 
 function execute()
 {
+	selectedPoints[0] = selection0.value;
+	selectedPoints[1] = selection1.value;
+	selectedPoints[2] = selection2.value;
+	selectedPoints[3] = selection3.value;
+
 	var currentTime = mainComp.time,
 		currentFrame = convertTimeToFPS(currentTime, fps),
-		direction = "Y",
+		direction = 1,
 		vertsEnd = [layer.property("Effects").property("Corner Pin").property("Upper Left"), 
 					layer.property("Effects").property("Corner Pin").property("Upper Right"),
 					layer.property("Effects").property("Corner Pin").property("Lower Left"),
@@ -74,79 +71,60 @@ function execute()
 					  vertsEnd[2].valueAtTime(currentTime,true),
 					  vertsEnd[3].valueAtTime(currentTime,true),
 					  ],
-		rotX = parseFloat(inputRotX.text),
-		rotY = parseFloat(inputRotY.text),
-		rotZ = parseFloat(inputRotZ.text)
+		rotAmount = parseFloat(inputRot.text)
 		;
 
-	// for (var j = 0; j < 2; j++)
-	// {
-		var centerPoint = vertsStart[3],
-			newCenterPoint = [ 0 , 0 ],
-			rotPoint = vertsStart[1],
-			dist = distance(centerPoint, rotPoint),
-			currentAngle = 0//Math.atan2(rotPoint[1] - centerPoint[1], rotPoint[0] - centerPoint[0]) * 180 / Math.PI + 90,
-			newCurrentAngle = 0,
-			newRotPoint = [ 0 , 0 ]
-			;
+	for (var j = 0; j < 4; j++)
+	{
+		if (selectedPoints[j] == true)
+		{	
+			var centerPoint = [ 0, 0 ],
+				rotPoint = [ 0, 0 ]
+				;
 
-		switch (direction)
-		{
-			case "X":
-				break;
+			if (j == 0 || j == 1)	// add direction modifier to this for rotation around other axes
+			{
+				rotPoint = vertsStart[j];
+				centerPoint = vertsStart[j + 2]; 
+			}
+			else if (j == 2 || j == 3)
+			{
+				rotPoint = vertsStart[j];
+				centerPoint = vertsStart[j - 2];
+			}
 
-			case "Y":
-				for (var i = 0; i <= rotY; i++)
-					{
-						var mult = 1;
+			var dist = distance(centerPoint, rotPoint),
+				currentAngle = Math.atan2(rotPoint[1] - centerPoint[1], rotPoint[0] - centerPoint[0]) * 180 / Math.PI + 90,
+				isoDist = Math.tan(degreesToRadians(30)) * isoWidth * 2
+				;
 
-						if (currentAngle < 90)
-						{
-							// newCurrentAngle = currentAngle - 15;
-							newCurrentAngle = reprojectAngle(currentAngle, 0, 90, -30, 90);
-							newRotPoint = rotPoint;
-							mult = .6;
-							newCenterPoint[0] = centerPoint[0] + isoWidth / 3;
-							newCenterPoint[1] = centerPoint[1] - dist / 2;
-						}
-						else if (currentAngle >= 90 && currentAngle < 180)
-						{
-							// newCurrentAngle = currentAngle - 90;
-							newCurrentAngle = reprojectAngle(currentAngle, 90, 180, 90, 150);						
-							newRotPoint = rotPoint;
-							mult = 1.775;
-							newCenterPoint[0] = centerPoint[0] - isoWidth + 1;
-							newCenterPoint[1] = centerPoint[1] - dist / 2;
-							
-						}
-						else if (currentAngle >= 180 && currentAngle < 270)
-						{
-							newCurrentAngle = reprojectAngle(currentAngle, 180, 270, 150, 270);
-							newRotPoint = rotPoint;
-							mult = .6;
-							newCenterPoint[0] = centerPoint[0] - isoWidth / 3 + 1;
-							newCenterPoint[1] = centerPoint[1] + dist / 2;	
-						}
-						else if (currentAngle >= 270 && currentAngle < 360)					
-						{
-							mult = 1.775;
-							newCurrentAngle = reprojectAngle(currentAngle, 270, 360, 270, 330);
-							newRotPoint = rotPoint;
-							newCenterPoint[0] = centerPoint[0] + isoWidth;
-							newCenterPoint[1] = centerPoint[1] + dist / 2;					
-						}
-						vertsEnd[1].setValueAtTime(convertFPSToTime(currentFrame + i, fps),rotateAroundPoint(0,newCenterPoint, dist, 0, newCurrentAngle, mult));
+			for (var i = 0; i <= Math.abs(rotAmount); i++)
+			{
+				if (currentAngle > 360)
+				{
+					currentAngle = 0;
+				}
 
-						currentAngle++;
-						//alert(newCurrentAngle);
-					}
-				break;
-			
-			case "Z":
-				break;
+				var newCenterPoint,
+					newCurrentAngle,
+					rotationValues
+					;
+
+				rotationValues = rotatePoint(currentAngle, centerPoint, dist, isoDist, isoWidth, direction); // incomplete
+
+				vertsEnd[j].setValueAtTime(convertFPSToTime(currentFrame + i, fps),rotateAroundPoint(0, rotationValues[0], dist, 0, rotationValues[1], rotationValues[2]));
+
+				if (rotAmount > 0)
+				{
+				currentAngle++;						
+				}
+				else 
+				{
+				currentAngle--;							
+				}
+			}
 		}
-	// }
-
+	}
 }
 
 function convertTimeToFPS(time, targetFPS)
@@ -172,7 +150,7 @@ function rotateAroundPoint(direction, _centerPoint, dist, startAngle, _currentAn
 		_rotPoint = [ 0 , 0 ]
 		;
 
-	_currentAngle = (_currentAngle - 90) / 57.5;
+	_currentAngle = degreesToRadians((_currentAngle - 90));
 	
 	_rotPoint[0] = _centerPoint[0] + (Math.cos(_currentAngle) * (dist * _mult));
 	_rotPoint[1] = _centerPoint[1] + (Math.sin(_currentAngle) * (dist * _mult));
@@ -194,26 +172,64 @@ function reprojectAngle(angle, min, max, _in, _out)
 		;
 
 	alpha = (angle - min) / (max - min);
-	//returnInterpolated = ;
 
 	returnInterpolated  = _in + (alpha * (_out - _in));
-
-	//alert(returnInterpolated);
 	return returnInterpolated;
-	//a + f * (b - a);
-	//(y1*(1-mu)+y2*mu);
 }
 
-	//alert(_currentAngle);
+function degreesToRadians(degrees)
+{
+	var returnRadians = degrees * Math.PI/180;
+	return returnRadians;
+}
 
+function rotatePoint(_currentAngle, _centerPoint, _dist, _isoDist, _isoWidth, axis)
+{
+	var limitsX = [ [ -30, 90 ], [ 90, 150 ], [ 150, 270 ], [ 270, 330 ] ],
+		limitsY = [ [ -30, 90 ], [ 90, 150 ], [ 150, 270 ], [ 270, 330 ] ],
+		limitsZ = [ [ -30, 90 ], [ 90, 150 ], [ 150, 270 ], [ 270, 330 ] ],
+		angleLimits = [ limitsX, limitsY, limitsZ ],
+		multX = [ [ .6 ], [ 1.75 ], [ .6 ], [ 1.775 ] ],
+		multY = [ [ .6 ], [ 1.75 ], [ .6 ], [ 1.775 ] ],
+		multZ = [ [ .6 ], [ 1.75 ], [ .6 ], [ 1.775 ] ],
+		multipliersXYZ = [ multX, multY, multZ ],
+		returnAngle = 0,
+		returnMult = 0,
+		returnCenterPoint = [ 0, 0 ]
+		;
 
-	// _currentAngle /= 57.5;
+	if (_currentAngle < 90)
+	{
+		// newCurrentAngle = currentAngle - 15;
+		returnAngle = reprojectAngle(_currentAngle, 0, 90, angleLimits[axis][0][0], angleLimits[axis][0][1]);
+		returnMult = multipliersXYZ[axis][0];
+		returnCenterPoint[0] = _centerPoint[0] + (((_dist / _isoDist) * _isoWidth) / 3) - (_dist / _isoDist);
+		returnCenterPoint[1] = _centerPoint[1] - _dist / 2;
+	}
+	else if (_currentAngle >= 90 && _currentAngle < 180)
+	{
+		// newCurrentAngle = currentAngle - 90;
+		returnAngle = reprojectAngle(_currentAngle, 90, 180, angleLimits[axis][1][0], angleLimits[axis][1][1]);						
+		returnMult = multipliersXYZ[axis][1];
+		returnCenterPoint[0] = _centerPoint[0] - (_dist / _isoDist) * _isoWidth - (_dist / _isoDist);
+		returnCenterPoint[1] = _centerPoint[1] - _dist / 2;
+		
+	}
+	else if (_currentAngle >= 180 && _currentAngle < 270)
+	{
+		returnAngle = reprojectAngle(_currentAngle, 180, 270, angleLimits[axis][2][0], angleLimits[axis][2][1]);
+		returnMult = multipliersXYZ[axis][2];
+		returnCenterPoint[0] = _centerPoint[0] - (((_dist / _isoDist) * _isoWidth) / 3) - (_dist / _isoDist);
+		returnCenterPoint[1] = _centerPoint[1] + _dist / 2;	
+	}
+	else if (_currentAngle >= 270 && _currentAngle < 360)					
+	{
+		returnAngle = reprojectAngle(_currentAngle, 270, 360, angleLimits[axis][3][0], angleLimits[axis][3][1]);
+		returnMult = multipliersXYZ[axis][3];
+		returnCenterPoint[0] = _centerPoint[0] + (_dist / _isoDist) * _isoWidth;
+		returnCenterPoint[1] = _centerPoint[1] + _dist / 2;					
+	}
 
-	// _rotPoint[0] = _centerPoint[0] + (Math.cos(_currentAngle - 30) * dist);
-	// _rotPoint[1] = _centerPoint[1] + (Math.sin(_currentAngle - 30) * dist);
-	// //alert(_rotPoint);
-	// //alert(Math.sin(_currentAngle) * 90);
-	// //endLocation[0] = _centerPoint[0] + (_rotPoint[0] - _centerPoint[0])*Math.cos(_currentAngle) - (_rotPoint[1] - _centerPoint[1])*Math.sin(_currentAngle);
-	// //endLocation[1] = _centerPoint[1] + (_rotPoint[1] - _centerPoint[1])*Math.sin(_currentAngle) + (_rotPoint[0] - _centerPoint[0])*Math.cos(_currentAngle);
-	// endLocation[0] = _centerPoint[0] + ((_rotPoint[0] - _centerPoint[0])*Math.cos(_currentAngle)*_mult - (_rotPoint[1] - _centerPoint[1])*Math.sin(_currentAngle)*_mult);
-	// endLocation[1] = _centerPoint[1] + ((_rotPoint[1] - _centerPoint[1])*Math.cos(_currentAngle)*_mult + (_rotPoint[0] - _centerPoint[0])*Math.sin(_currentAngle)*_mult);
+	var returns = [ returnCenterPoint, returnAngle, returnMult ];
+	return returns;
+}
